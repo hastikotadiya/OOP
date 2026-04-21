3236f88316e3dd2e534efbc31746f85b6bde7da6
hastikotadiya <hastikotadiya01@gmail.com>
2026-04-21 15:58:02 +0530

    Create pr1.java

---
+++ pr1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class pr1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter distance in meters: ");
+        double meters = sc.nextDouble();
+        double feet = meters * 3.28084;
+        System.out.printf("Distance in feet: %.2f\n", feet);
+    }
+}
+//output
+// Enter distance in meters: 10
+// Distance in feet: 32.81
