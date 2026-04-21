dfc9a31df6bc419abbd1f695022ea0ebeff67285
hastikotadiya <hastikotadiya01@gmail.com>
2026-04-21 16:16:07 +0530

    Create pr3.java

---
+++ pr3.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class pr3{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a single letter: ");
+        char ch = sc.next().charAt(0);
+        ch = Character.toLowerCase(ch);
+        if (Character.isLetter(ch)) {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                System.out.println("It is a vowel.");
+            } else {
+                System.out.println("It is a consonant.");
+            }
+        } else {
+            System.out.println("Invalid input! Please enter a letter.");
+        }
+    }
+}
+// output
+// Enter a single letter: A
+// It is a vowel.
