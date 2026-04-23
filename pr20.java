840f76323f6abffec6fc87e39791f81eab24761b
hastikotadiya <hastikotadiya01@gmail.com>
2026-04-23 16:32:39 +0530

    Create pr20.java

---
+++ pr20.java
@@ -0,0 +1,27 @@
+public class pr20{
+    public static void main(String[] args) {
+        try {
+            if (args.length < 2) {
+                throw new ArrayIndexOutOfBoundsException("Please provide numerator and denominator");
+            }
+            int numerator = Integer.parseInt(args[0]);
+            int denominator = Integer.parseInt(args[1]);
+            int result = numerator / denominator;
+            System.out.println("Numerator: " + numerator);
+            System.out.println("Denominator: " + denominator);
+            System.out.println("Result: " + result);
+        } 
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error: Missing command-line arguments!");
+        } 
+        catch (NumberFormatException e) {
+            System.out.println("Error: Please enter valid integers!");
+        } 
+        catch (ArithmeticException e) {
+            System.out.println("Error: Cannot divide by zero!");
+        } 
+        catch (Exception e) {
+            System.out.println("Unexpected Error: " + e);
+        }
+    }
+}
